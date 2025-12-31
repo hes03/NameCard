240c8173e3f73278b1163c63424f98e2eccfa402
dmstn <eunsoo4629@naver.com>
2025-12-31 11:28:30 +0900

    라우터 적용완료

--- nameCard-step1/src/App.jsx
+++ nameCard-step1/src/App.jsx
@@ -1,10 +1,25 @@
-import './App.css';
+import { Route, Routes } from 'react-router';
+import styled from 'styled-components';
 
+const AppDiv = styled.div`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #e8e7e6;
+`
 const App = () => {
+  //선언부
   return (
-    <div className="App">
-      Hello React!!!
-    </div>
+    <>
+      <AppDiv>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/maker" element={<Maker />} />
+        </Routes>
+      </AppDiv>
+    </>
   );
 }
 
