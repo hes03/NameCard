ce84378f5754a9164b7205c60941d0e5ac4f35ef
dmstn <eunsoo4629@naver.com>
2026-01-06 11:34:57 +0900

    이미지 파일업로드

--- nameCard-step1/src/index.js
+++ nameCard-step1/src/index.js
@@ -8,8 +8,12 @@ import ImageFileInput from './components/common/ImageFileInput';
 const imageUploader = new ImageUploader()
 // 왜 CardEditor에서 사용할 컴포넌트를 index.js에 선언하였나?
 // props는 상위 컴포넌트에서 하위 컴포넌트로만 전달가능함.
+// () => ()
+// () => {return}
+// ...spread operation
+// 이거가 없다면 새로고침이 된다. 누적이 안됨. -> 덮어쓰기와 같다.
 const FileInput = props => (
-  <ImageFileInput {...props} ImageUploader={imageUploader} />
+  <ImageFileInput {...props} imageUploader={imageUploader} />
 )
 // public > index.html div 위치 파악
 const root = ReactDOM.createRoot(document.querySelector('#root'));
